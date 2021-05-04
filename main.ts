810dcb1c9d80f0cc9d6146f3e020fbb31c468488
David Kajpust <kajpustd@gmail.com>
2021-05-03 19:35:33 -0500

    Implemtation that works with bn and decimal

--- main.ts
+++ main.ts
@@ -1,31 +1,32 @@
 // import { bigNumberify } from "helpers";
-// import Decimal from 'decimal.js'
+import Decimal from 'decimal.js'
 import { BigNumber, ethers, utils } from 'ethers'
 
 import { nSignalToTokens, tokensToNSignal, signalToTokens, tokensToSignal } from './src/src'
-import exampleQuery from './example-query.json'
+import { exampleQueryResult } from './example-query'
 
+const result = exampleQueryResult.data
 const ONE_HUNDRED_THOUSAND_E18 = BigNumber.from('100000000000000000000000')
 const ONE_HUNDRED_E18 = BigNumber.from('100000000000000000000')
 
-// Decimal.set({ precision: 30, rounding: Decimal.ROUND_DOWN })
+Decimal.set({ toExpPos: 28, rounding: Decimal.ROUND_DOWN })
 
 // TODO - grab a delegators address, so we could do BURN too, would need to read name signal
 
 // EXAMPLE QUERY - GLOBAL VALUES
-const CURATION_TAX = BigNumber.from(exampleQuery.data.graphNetwork.curationTaxPercentage)
+const CURATION_TAX = BigNumber.from(result.graphNetwork.curationTaxPercentage)
 const MINIMUM_CURATION_DEPOSIT = BigNumber.from(
-  exampleQuery.data.graphNetwork.minimumCurationDeposit,
+  result.graphNetwork.minimumCurationDeposit,
 )
 
 ///// EXAMPLE QUERY - SUBGRAPH SPECIFIC VALUES
-const SUBGRAPH_DEPLOYMENT = exampleQuery.data.subgraph.currentVersion.subgraphDeployment
+const SUBGRAPH_DEPLOYMENT = result.subgraph.currentVersion.subgraphDeployment
 const tokensCuratedOnDeployment = BigNumber.from(SUBGRAPH_DEPLOYMENT.signalledTokens)
 const totalVSignal = BigNumber.from(SUBGRAPH_DEPLOYMENT.signalAmount)
-const reserveRatio = SUBGRAPH_DEPLOYMENT.reserveRatio // no BigNumber
-const totalNSignal = BigNumber.from(exampleQuery.data.subgraph.nameSignalAmount)
-const vSignalGNS = BigNumber.from(exampleQuery.data.signal.signal)
-const userNSignal = BigNumber.from(exampleQuery.data.nameSignal.nameSignal)
+const reserveRatio = BigNumber.from(SUBGRAPH_DEPLOYMENT.reserveRatio)
+const totalNSignal = BigNumber.from(result.subgraph.nameSignalAmount)
+const vSignalGNS = BigNumber.from(result.signal.signal)
+const userNSignal = BigNumber.from(result.nameSignal.nameSignal)
 
 function prettyPrintBNs(bns: Array<BigNumber>): void {
   bns.forEach((element) => {
